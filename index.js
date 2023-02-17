6fa7c9972923c5577e8eafd704a46f21367176c0
hovinhthanh7893 <hovinhthanh7893@gmail.com>
2023-02-17 14:01:39 +0100

    cursor

--- index.js
+++ index.js
@@ -1,3 +1,56 @@
+//Mouse
+let cursor = document.querySelector(".cursor");
+document.querySelector("body").addEventListener("mousemove", function(event) {
+  cursor.style.left = event.clientX + "px"; 
+  cursor.style.top = event.clientY + "px";
+});
+//Mouse change color - sections
+document.querySelector("#section1").addEventListener("mouseover", function(event) {
+  cursor.classList.add("cursor1");
+  cursor.classList.remove("cursor2");
+  cursor.classList.remove("cursor3");
+  cursor.classList.remove("cursor4");
+  document.querySelector("#navRight1").style.fontStyle = "normal";
+  document.querySelector("#navRight2").style.fontStyle = "normal";
+  document.querySelector("#navRight3").style.fontStyle = "normal";
+});
+document.querySelector("#section2").addEventListener("mouseover", function(event) {
+  cursor.classList.add("cursor2");
+  cursor.classList.remove("cursor1");
+  cursor.classList.remove("cursor3");
+  cursor.classList.remove("cursor4");
+  document.querySelector("#navRight1").style.fontStyle = "italic";
+  document.querySelector("#navRight2").style.fontStyle = "normal";
+  document.querySelector("#navRight3").style.fontStyle = "normal";
+});
+document.querySelector("#section3").addEventListener("mouseover", function(event) {
+  cursor.classList.add("cursor3");
+  cursor.classList.remove("cursor1");
+  cursor.classList.remove("cursor2");
+  cursor.classList.remove("cursor4");
+  document.querySelector("#navRight1").style.fontStyle = "normal";
+  document.querySelector("#navRight2").style.fontStyle = "italic";
+  document.querySelector("#navRight3").style.fontStyle = "normal";
+});
+document.querySelector("#section4").addEventListener("mouseover", function(event) {
+  cursor.classList.add("cursor4");
+  cursor.classList.remove("cursor1");
+  cursor.classList.remove("cursor2");
+  cursor.classList.remove("cursor3");
+  document.querySelector("#navRight1").style.fontStyle = "normal";
+  document.querySelector("#navRight2").style.fontStyle = "normal";
+  document.querySelector("#navRight3").style.fontStyle = "italic";
+});
+//Mouse change - navbar
+document.querySelectorAll("a").forEach((each) => {
+  each.addEventListener("mouseover", function(event) {
+    cursor.classList.add("cursorHover");
+  });
+  each.addEventListener("mouseout", function(event) {
+    cursor.classList.remove("cursorHover");
+  });
+});
+
 //Change banner image
 let bannerPic = 0;
 let currentBanner = 0;
@@ -6,6 +59,18 @@ function slideShowBanner() {
         bannerPic = 1;
     } else {
         bannerPic +=1;
+    }
+    if (currentBanner !== bannerPic) {
+      document.querySelectorAll(".bannerImg")[bannerPic-1].style.opacity = "1";
+      document.querySelectorAll(".bannerTitle")[bannerPic-1].style.opacity = "1";
+      if (bannerPic === 1) {
+          document.querySelectorAll(".bannerImg")[4].style.opacity = "0";
+          document.querySelectorAll(".bannerTitle")[4].style.opacity = "0";
+      } else {
+          document.querySelectorAll(".bannerImg")[bannerPic-2].style.opacity = "0";
+          document.querySelectorAll(".bannerTitle")[bannerPic-2].style.opacity = "0";
+      }
+      currentBanner = bannerPic;
     }
     setTimeout(slideShowBanner, 5000);
 }
@@ -37,21 +102,3 @@ document.querySelector("#navRight2").addEventListener("click", function(event) {
 document.querySelector("#navRight3").addEventListener("click", function(event) {
   document.querySelector("nav").style.transform = 'translateY(-100px)';
 });
-
-//Animation
-const animate = () => {
-    if (currentBanner !== bannerPic) {
-        document.querySelectorAll(".bannerImg")[bannerPic-1].style.opacity = "1";
-        document.querySelectorAll(".bannerTitle")[bannerPic-1].style.opacity = "1";
-        if (bannerPic === 1) {
-            document.querySelectorAll(".bannerImg")[4].style.opacity = "0";
-            document.querySelectorAll(".bannerTitle")[4].style.opacity = "0";
-        } else {
-            document.querySelectorAll(".bannerImg")[bannerPic-2].style.opacity = "0";
-            document.querySelectorAll(".bannerTitle")[bannerPic-2].style.opacity = "0";
-        }
-        currentBanner = bannerPic;
-    }
-    requestAnimationFrame(animate);
-};
-animate();
