c0f0bf9a1add9dccf19d62d4cc53b08a3f784081
hovinhthanh7893 <hovinhthanh7893@gmail.com>
2023-02-18 01:25:56 +0100

    projects

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-//Mouse
+//MOUSE CURSOR
 let cursor = document.querySelector(".cursor");
 document.querySelector("body").addEventListener("mousemove", function(event) {
   cursor.style.left = event.clientX + "px"; 
   cursor.style.top = event.clientY + "px";
 });
-//Mouse change color - sections
+//MOUSE CHANGE COLOR - SECTIONS
 document.querySelector("#section1").addEventListener("mouseover", function(event) {
   cursor.classList.add("cursor1");
   cursor.classList.remove("cursor2");
   cursor.classList.remove("cursor3");
   cursor.classList.remove("cursor4");
   document.querySelector("#navRight1").style.fontStyle = "normal";
+  document.querySelector("#navRight1").classList.remove("is-on");
   document.querySelector("#navRight2").style.fontStyle = "normal";
+  document.querySelector("#navRight2").classList.remove("is-on");
   document.querySelector("#navRight3").style.fontStyle = "normal";
+  document.querySelector("#navRight3").classList.remove("is-on");
 });
 document.querySelector("#section2").addEventListener("mouseover", function(event) {
   cursor.classList.add("cursor2");
@@ -20,8 +23,11 @@ document.querySelector("#section2").addEventListener("mouseover", function(event
   cursor.classList.remove("cursor3");
   cursor.classList.remove("cursor4");
   document.querySelector("#navRight1").style.fontStyle = "italic";
+  document.querySelector("#navRight1").classList.add("is-on");
   document.querySelector("#navRight2").style.fontStyle = "normal";
+  document.querySelector("#navRight2").classList.remove("is-on");
   document.querySelector("#navRight3").style.fontStyle = "normal";
+  document.querySelector("#navRight3").classList.remove("is-on");
 });
 document.querySelector("#section3").addEventListener("mouseover", function(event) {
   cursor.classList.add("cursor3");
@@ -29,8 +35,11 @@ document.querySelector("#section3").addEventListener("mouseover", function(event
   cursor.classList.remove("cursor2");
   cursor.classList.remove("cursor4");
   document.querySelector("#navRight1").style.fontStyle = "normal";
+  document.querySelector("#navRight1").classList.remove("is-on");
   document.querySelector("#navRight2").style.fontStyle = "italic";
+  document.querySelector("#navRight2").classList.add("is-on");
   document.querySelector("#navRight3").style.fontStyle = "normal";
+  document.querySelector("#navRight3").classList.remove("is-on");
 });
 document.querySelector("#section4").addEventListener("mouseover", function(event) {
   cursor.classList.add("cursor4");
@@ -38,10 +47,13 @@ document.querySelector("#section4").addEventListener("mouseover", function(event
   cursor.classList.remove("cursor2");
   cursor.classList.remove("cursor3");
   document.querySelector("#navRight1").style.fontStyle = "normal";
+  document.querySelector("#navRight1").classList.remove("is-on");
   document.querySelector("#navRight2").style.fontStyle = "normal";
+  document.querySelector("#navRight2").classList.remove("is-on");
   document.querySelector("#navRight3").style.fontStyle = "italic";
+  document.querySelector("#navRight3").classList.add("is-on");
 });
-//Mouse change - links
+//MOUSE CHANGE STYLE - LINKS
 document.querySelectorAll("a").forEach((each) => {
   each.addEventListener("mouseover", function(event) {
     cursor.classList.add("cursorHover");
@@ -50,12 +62,24 @@ document.querySelectorAll("a").forEach((each) => {
     cursor.classList.remove("cursorHover");
   });
 });
+//MOUSE CHANGE STYLE - BUTTONS
+document.querySelectorAll("button").forEach((each) => {
+  each.addEventListener("mouseover", function(event) {
+    cursor.classList.add("cursorHover");
+  });
+  each.addEventListener("mouseout", function(event) {
+    cursor.classList.remove("cursorHover");
+  });
+});
 
-//Change banner image
+
+
+//BANNER IMAGE SLIDE
+const totalImage = 3
 let bannerPic = 0;
 let currentBanner = 0;
 function slideShowBanner() {
-    if (bannerPic === 3) {
+    if (bannerPic === totalImage) {
         bannerPic = 1;
     } else {
         bannerPic +=1;
@@ -64,8 +88,8 @@ function slideShowBanner() {
       document.querySelectorAll(".bannerImg")[bannerPic-1].style.opacity = "1";
       document.querySelectorAll(".bannerTitle")[bannerPic-1].style.opacity = "1";
       if (bannerPic === 1) {
-          document.querySelectorAll(".bannerImg")[2].style.opacity = "0";
-          document.querySelectorAll(".bannerTitle")[2].style.opacity = "0";
+          document.querySelectorAll(".bannerImg")[totalImage-1].style.opacity = "0";
+          document.querySelectorAll(".bannerTitle")[totalImage-1].style.opacity = "0";
       } else {
           document.querySelectorAll(".bannerImg")[bannerPic-2].style.opacity = "0";
           document.querySelectorAll(".bannerTitle")[bannerPic-2].style.opacity = "0";
@@ -75,6 +99,8 @@ function slideShowBanner() {
     setTimeout(slideShowBanner, 5000);
 }
 slideShowBanner();
+
+
 
 //Hide-show navbar
 document.body.addEventListener('wheel', checkScrollDirection);
@@ -92,6 +118,8 @@ function checkScrollDirectionIsUp(event) {
   return event.deltaY < 0;
 }
 
+
+
 //COLLAPSE SUBMENU ON CHOOSE
 document.querySelector("#navRight1").addEventListener("click", function(event) {
   document.querySelector("nav").style.transform = 'translateY(-100px)';
@@ -102,3 +130,63 @@ document.querySelector("#navRight2").addEventListener("click", function(event) {
 document.querySelector("#navRight3").addEventListener("click", function(event) {
   document.querySelector("nav").style.transform = 'translateY(-100px)';
 });
+
+
+
+//PROJECT SLIDE
+const totalProject = document.querySelectorAll(".project").length;
+let slidePos = 0;
+let currentProject = Math.ceil(totalProject / 2);
+document.querySelectorAll(".project")[currentProject-1].classList.add("is-in");
+document.querySelectorAll(".dot")[currentProject-1].classList.add("is-at");
+//Next
+document
+  .querySelector(".nextBut")
+  .addEventListener("click", function(event) {
+    document.querySelectorAll(".project")[currentProject-1].classList.remove("is-in");
+    document.querySelectorAll(".dot")[currentProject-1].classList.remove("is-at");
+    if (currentProject === totalProject) {
+      slidePos = -(slidePos);
+      currentProject = 1;
+      document.querySelector(".wrapper").style.translate = slidePos + "px";
+      document.querySelectorAll(".project")[currentProject-1].classList.add("is-in");
+      document.querySelectorAll(".dot")[currentProject-1].classList.add("is-at");
+    } else {
+      slidePos = slidePos - document.querySelector(".wrapper").getBoundingClientRect().width / totalProject;
+      currentProject = currentProject + 1;
+      document.querySelector(".wrapper").style.translate = slidePos + "px";
+      document.querySelectorAll(".project")[currentProject-1].classList.add("is-in");
+      document.querySelectorAll(".dot")[currentProject-1].classList.add("is-at");
+    }
+});
+//Previous
+document
+  .querySelector(".prevBut")
+  .addEventListener("click", function(event) {
+    document.querySelectorAll(".project")[currentProject-1].classList.remove("is-in");
+    document.querySelectorAll(".dot")[currentProject-1].classList.remove("is-at");
+    if (currentProject === 1) {
+      slidePos = -(slidePos);
+      currentProject = totalProject;
+      document.querySelector(".wrapper").style.translate = slidePos + "px";
+      document.querySelectorAll(".project")[currentProject-1].classList.add("is-in");
+      document.querySelectorAll(".dot")[currentProject-1].classList.add("is-at");
+    } else {
+      slidePos = document.querySelector(".wrapper").getBoundingClientRect().width / totalProject + slidePos;
+      currentProject = currentProject - 1;
+      document.querySelector(".wrapper").style.translate = slidePos + "px";
+      document.querySelectorAll(".project")[currentProject-1].classList.add("is-in");
+      document.querySelectorAll(".dot")[currentProject-1].classList.add("is-at");
+    }
+});
+//Refresh on resize
+function onWindowResize() {
+  document.querySelectorAll(".project")[currentProject-1].classList.remove("is-in");
+  document.querySelectorAll(".dot")[currentProject-1].classList.remove("is-at");
+  slidePos = 0;
+  currentProject = Math.ceil(totalProject / 2);
+  document.querySelector(".wrapper").style.translate = slidePos + "px";
+  document.querySelectorAll(".project")[currentProject-1].classList.add("is-in");
+  document.querySelectorAll(".dot")[currentProject-1].classList.add("is-at");
+}
+window.addEventListener("resize", onWindowResize, false);
